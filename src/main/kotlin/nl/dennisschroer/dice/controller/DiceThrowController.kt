8d20d4485c9c41d24ffcc4698c5ca458d1728dab
Dennis Schroer <dev@dennisschroer.nl>
2020-10-18 17:50:44 +0200

    Log throws

--- src/main/kotlin/nl/dennisschroer/dice/controller/DiceThrowController.kt
+++ src/main/kotlin/nl/dennisschroer/dice/controller/DiceThrowController.kt
@@ -3,6 +3,7 @@ package nl.dennisschroer.dice.controller
 import nl.dennisschroer.dice.message.DiceThrow
 import nl.dennisschroer.dice.message.DiceThrowIntent
 import nl.dennisschroer.dice.model.Die
+import org.slf4j.LoggerFactory
 import org.springframework.messaging.handler.annotation.MessageMapping
 import org.springframework.messaging.handler.annotation.SendTo
 import org.springframework.stereotype.Controller
@@ -10,10 +11,14 @@ import kotlin.random.Random
 
 @Controller
 class DiceThrowController {
+    private val log = LoggerFactory.getLogger(javaClass)
+
     @MessageMapping("/throw")
     @SendTo("/topic/throws")
     fun throwDice(intent: DiceThrowIntent): DiceThrow {
-        return DiceThrow(intent.name, intent.dice.take(10).map { die -> Pair(die, throwDie(die)) })
+        val diceThrow = DiceThrow(intent.name, intent.dice.take(10).map { die -> Pair(die, throwDie(die)) })
+        log.info(diceThrow.toString())
+        return diceThrow
     }
 
     private fun throwDie(die: Die): Int = Random.nextInt(0, die.sides) + 1
